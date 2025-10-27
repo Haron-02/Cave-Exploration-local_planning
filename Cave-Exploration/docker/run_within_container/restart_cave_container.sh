c99d95675b6bb2e712284db057119af0bdd978cb
Dreamcatcher <131952186+Haron-02@users.noreply.github.com>
2025-10-27 19:54:48 +0800

    Delete Cave-Exploration directory

--- Cave-Exploration/docker/run_within_container/restart_cave_container.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CONTAINER_NAME=cave_exp
-
-xhost +
-
-sudo docker start $CONTAINER_NAME
-sudo docker exec -it $CONTAINER_NAME /bin/bash
--- Cave-Exploration/fael_planner/planner/src/tsp_solver/two_opt.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by hjl on 2022/6/7.
-//
-
-#include "../../include/tsp_solver/two_opt.h"
